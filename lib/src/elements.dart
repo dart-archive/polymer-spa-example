6b88e191e826bf4cece5447e2fe411fde1022c04
Jacob MacDonald <jakemac@google.com>
2015-03-06 11:23:51 -0800

    first cut
    R=sigmund@google.com
    
    Review URL: https://codereview.chromium.org//975823002

---
+++ lib/src/elements.dart
@@ -0,0 +1,18 @@
+// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// Packages up all the elements that we use in our app into a single import.
+library polymer_core_and_paper_examples.spa.elements;
+
+export 'package:core_elements/core_ajax_dart.dart';
+export 'package:core_elements/core_a11y_keys.dart';
+export 'package:core_elements/core_animated_pages.dart';
+export 'package:core_elements/core_animated_pages/transitions/slide_from_right.dart';
+export 'package:core_elements/core_icon.dart';
+export 'package:core_elements/core_icon_button.dart';
+export 'package:core_elements/core_menu.dart';
+export 'package:core_elements/core_scaffold.dart';
+export 'package:core_elements/core_toolbar.dart';
+export 'package:core_elements/roboto.dart';
+export 'package:paper_elements/paper_item.dart';
