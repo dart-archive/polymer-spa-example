be3ef50ffd6d68b6c9352460ecf2536dd6eb8755
Hadrien Lejard <hadrien.lejard@gmail.com>
2015-09-29 12:17:51 +0200

    update pubspec

---
+++ web/index.dart
@@ -0,0 +1,10 @@
+/**
+ * Created by lejard_h on 27/09/15.
+ */
+
+import 'package:polymer/polymer.dart';
+import "package:polymer_spa_example/example_app.dart";
+
+main() async {
+  await initPolymer();
+}
